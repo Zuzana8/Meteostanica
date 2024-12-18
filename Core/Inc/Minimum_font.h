4cc1d491c19904b1c85beafcbf0d925ff588ea1d
xszarkad1 <xszarkad1@stuba.sk>
2024-12-18 19:59:29 +0100

    Final display

---
+++ Core/Inc/Minimum_font.h
@@ -0,0 +1,14 @@
+#ifndef MINIMUM_FONT_H_
+#define MINIMUM_FONT_H_
+
+// Index definitions for weather symbols
+#define SUNNY_SYMBOL_INDEX 128
+#define CLOUDY_SYMBOL_INDEX 129
+#define RAINY_SYMBOL_INDEX 130
+
+#define FONTUS_SIZE 131
+
+extern const unsigned char fontus[][6];
+
+
+#endif /* MINIMUM_FONT_H_ */
