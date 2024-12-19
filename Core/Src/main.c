be66b94b181a819d4ad047f740b99788614390d9
Dorottya Szarka <125996769+xszarkad1@users.noreply.github.com>
2024-12-19 01:48:27 +0100

    Last resort (#8)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,28 +6,30 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "fatfs.h"
+#include "spi.h"
+#include "gpio.h"
+#include "ili9163.h"
+#include "rtc.h"
+#include <time.h>
+#include <stdlib.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "lps25hb.h"
-#include "hts221.h"
-#include "i2c.h"
-#include "zambretti.h"
 
-#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,44 +49,59 @@
 
 /* Private variables ---------------------------------------------------------*/
 
-SPI_HandleTypeDef hspi1;
-
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-//static void MX_I2C1_Init(void);
-static void MX_SPI1_Init(void);
+void TempHumPres(float temperature, float humidity, int pressure);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-float temperature = 0;
-float humidity = 0;
-float pressure = 0;
-char z_text[50];
+uint8_t rx_data = 0;
+uint8_t switchState = 0; // 0 = main program, 1 = graph
+float temperatureHistory[24 * 7] = {0}; // Array for 7 days of hourly temperatures
+uint8_t graphDrawn = 0;
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
+
+float getTemperature(void){
+
+	return (rand() % 21) + 10;
+}
+
+float getHumidity(void){
+
+	return (rand() % 70) + 10;
+}
+
+int getPressure(void){
+	return (rand() % 75) + 980;
+}
+
 int main(void)
 {
-
+	srand((unsigned int)time(NULL));
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
 
+
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  /* System interrupt init*/
 
   /* USER CODE BEGIN Init */
 
@@ -99,33 +116,90 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
   MX_SPI1_Init();
-  MX_FATFS_Init();
-  /* USER CODE BEGIN 2 */
 
-  //initialize sensors
-  HTS221_init();
-  LPS25HB_init();
+  initCD_Pin();
+  initCS_Pin();
+  initRES_Pin();
 
-  /* USER CODE END 2 */
+  LL_mDelay(50);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	  float temp = hts221_get_temperature();
-	  float hum = hts221_get_humidity();
-	  float press = lps25hb_get_pressure();
-	  zambretti(press, temp, z_text);
-	  //calculate prediction
-	  char text[50];
-	  strcpy(text, z_text);
+  lcdInitialise(0x48);
+  lcdClearDisplay(0xFFFF);
 
-	  LL_mDelay(1000);
-    /* USER CODE BEGIN 3 */
+  RTC_Init();
+//  lcdPutS("VRS 2019", lcdTextX(2), lcdTextY(1), 0x0000, 0xFFFF);
+//  lcdPutS("Cvicenie 8", lcdTextX(2), lcdTextY(4), 0x0000, 0xFFFF);
+//  lcdPutS("SPI komunikacia", lcdTextX(2), lcdTextY(5), 0x0000, 0xFFFF);
+
+
+  for (int i = 0; i < 8 * 7; i++) {
+      temperatureHistory[i] = (rand() % 21) + 10; // Random temperatures between 10 and 30 degrees
   }
+
+  	uint8_t lastWeather = 0;
+	while (1)
+	{
+		if (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_3)) {
+		    printf("Button pressed!\n");
+		}
+		//switchState = LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_1); // Replace with your switch GPIO pin
+
+		if (switchState){
+			if (!graphDrawn) {
+		    // Draw the graph
+		    drawTemperatureGraph(temperatureHistory, 8 * 7);
+		    LL_mDelay(1000); // Avoid rapid refresh of the graph
+		    graphDrawn = 1;
+			}
+		} else {
+
+
+			if (graphDrawn == 1){
+				lcdClearDisplay(decodeRgbValue(0xFF, 0xFF, 0xFF));
+				graphDrawn = 0;
+			}
+
+
+			float temperature = getTemperature();
+			float humidity = getTemperature();
+			float pressure = getPressure();
+
+			uint8_t currentWeather;
+
+			// Determine current weather based on pressure and humidity
+			if (pressure > 1013.25 && humidity < 60.0) //1013.25 je priemerny tlak vo vyske 1500m
+			{
+			// High pressure and low humidity indicate sunny weather
+			currentWeather = SUNNY_SYMBOL_INDEX;
+			}
+			else if (pressure < 1000.0 && humidity > 70.0)
+			{
+			// Low pressure and high humidity indicate rainy weather
+			currentWeather = RAINY_SYMBOL_INDEX;
+			}
+			else
+			{
+			// Otherwise, assume cloudy weather
+			currentWeather = CLOUDY_SYMBOL_INDEX;
+		}
+
+
+			if (currentWeather != lastWeather)
+				{
+					drawWeather(currentWeather);
+					lastWeather = currentWeather; // Update the last displayed weather
+				}
+
+			get_current_time();
+
+			display_time_with_large_digits();
+
+			TempHumPres(temperature, humidity, pressure);
+
+			LL_mDelay(500);
+		}
+	}
   /* USER CODE END 3 */
 }
 
@@ -135,118 +209,58 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
   {
-    Error_Handler();
+  Error_Handler();
   }
+  LL_RCC_HSI_Enable();
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
-  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  LL_RCC_HSI_SetCalibTrimming(16);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
   {
-    Error_Handler();
+
   }
+  LL_Init1msTick(8000000);
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+  LL_SetSystemCoreClock(8000000);
 }
 
-
-/**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI1_Init(void)
+void TempHumPres(float temperature, float humidity, int pressure)
 {
+    char buffer[10];
 
-  /* USER CODE BEGIN SPI1_Init 0 */
+    // Display the static labels for temperature and humidity
+    lcdPutLargeS("Tem.:", 10, 90, 0x0000, 0xFFFF);       // Black text on white background
+    lcdPutLargeS("Hum.:", 170, 90, 0x0000, 0xFFFF);    // Black text on white background
+    lcdPutLargeS("Pres.:", 10, 75, 0x0000, 0xFFFF);    // Black text on white background
 
-  /* USER CODE END SPI1_Init 0 */
+    //dlhodobá predpoveď
+    lcdPutLargeS("Fairly Fine Showery     Later", 10, 140, 0x0000, 0xFFFF);
 
-  /* USER CODE BEGIN SPI1_Init 1 */
+    // Display the temperature value
+    snprintf(buffer, sizeof(buffer), "%.1f C", temperature);
+    lcdPutLargeS(buffer, 70, 90, 0x0000, 0xFFFF);   // Display temperature in large font
 
-  /* USER CODE END SPI1_Init 1 */
-  /* SPI1 parameter configuration*/
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_4BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 7;
-  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI1_Init 2 */
+    // Display the humidity value
+    snprintf(buffer, sizeof(buffer), "%.1f %%", humidity);
+    lcdPutLargeS(buffer, 230, 90, 0x0000, 0xFFFF);   // Display humidity in large font
 
-  /* USER CODE END SPI1_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, DISPLAY_CS_Pin|SD_CS_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : Button___ext__interrupt_Pin */
-  GPIO_InitStruct.Pin = Button___ext__interrupt_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(Button___ext__interrupt_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : DISPLAY_CS_Pin SD_CS_Pin */
-  GPIO_InitStruct.Pin = DISPLAY_CS_Pin|SD_CS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+    // Display the humidity value
+    snprintf(buffer, sizeof(buffer), "%d hPa", pressure);
+    lcdPutLargeS(buffer, 80, 75, 0x0000, 0xFFFF);   // Display humidity in large font
 }
 
 /* USER CODE BEGIN 4 */
@@ -261,12 +275,24 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+
   /* USER CODE END Error_Handler_Debug */
 }
+
+/* USER CODE BEGIN 0 */
+void EXTI3_IRQHandler(void)
+{
+
+        if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_3))
+        {
+            LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_3); // Clear the interrupt flag
+            switchState = !switchState; // Toggle switch state
+        }
+
+}
+
+/* USER CODE END 0 */
+
 
 #ifdef  USE_FULL_ASSERT
 /**
@@ -276,11 +302,13 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
+void assert_failed(char *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
